638220ad91a7734836412599d9eb3e6e2e78e16a
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-03 19:10:03 -0400

    Add files via upload

---
+++ MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+        }   int difference = 10 - 5;
+            Console.WriteLine("Ten minus Five = " + difference.ToString());
+            Console.ReadLine();
+
+            int product = 10 * 5;
+            Console.WriteLine(product);
+            Console.ReadLine();
+
+            int quotient = 100 / 5;
+            Console.WriteLine (quotient);
+            Console.ReadLine();
+
+            int remainder = 11 % 2;
+            Console.WriteLine(remainder);
+            Console.ReadLine();
+
+
+    }
+}
