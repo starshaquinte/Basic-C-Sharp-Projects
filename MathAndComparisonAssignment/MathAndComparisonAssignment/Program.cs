6ad7fc9ce2848f25acf6f4f2bce7279e981d084a
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-04 16:45:22 -0400

    Add files via upload

--- MathAndComparisonAssignment/MathAndComparisonAssignment/Program.cs
+++ MathAndComparisonAssignment/MathAndComparisonAssignment/Program.cs
@@ -6,15 +6,15 @@ namespace MathAndComparisonAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Anonymous Income Comparison Program"); //Introduction to program 
             Console.ReadLine();
 
             Console.WriteLine("Person 1");
             Console.ReadLine();
 
-            Console.WriteLine("Hourly Rate?");
+            Console.WriteLine("Hourly Rate?"); 
             int hourlyRate1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Your hourly rate is: " + hourlyRate1);
+            Console.WriteLine("Your hourly rate is: " + hourlyRate1);  //Retrieves following details from person 1 input hourly rate and hours worked per week.
 
             Console.WriteLine("Hours worked per week?");
             int hoursWorked1 = Convert.ToInt32(Console.ReadLine());
@@ -25,28 +25,23 @@ namespace MathAndComparisonAssignment
 
             Console.WriteLine("Hourly Rate?");
             int hourlyRate2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Your hourly rate is: " + hourlyRate2);
+            Console.WriteLine("Your hourly rate is: " + hourlyRate2);//Retrieves following details from person 1 input hourly rate and hours worked per week.
 
             Console.WriteLine("Hours worked per week?");
             int hoursWorked2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Your hours worked per week is: " + hoursWorked2);
 
             Console.WriteLine("Annual salary of Person 1");
-            int totalSalary1 = (hourlyRate1 * hoursWorked1)52;
-            Console.WriteLine("Annual salary of Person 1: " + totalSalary1);
+            int totalSalary1 = (hourlyRate1 * hoursWorked1)*52;
+            Console.WriteLine("Annual salary of Person 1: " + totalSalary1); //Annual salary of person 1
 
             Console.WriteLine("Annual salary of Person 2");
-            int totalSalary2 = (hourlyRate2 * hoursWorked2)52;
-            Console.WriteLine("Annual salary of Person 2: " + totalSalary2);
+            int totalSalary2 = (hourlyRate2 * hoursWorked2)*52;
+            Console.WriteLine("Annual salary of Person 2: " + totalSalary2); //Annual salary of person 2 
 
-            int makesmore = Convert.ToInt32(Console.ReadLine());
-            bool trueOrFalse = makesmore(Person1 > Person2);
+            bool trueOrFalse = (totalSalary1 > totalSalary2);
             Console.WriteLine(trueOrFalse.ToString());
-            Console.ReadLine();
-
-               
-
-
+            Console.ReadLine(); //comparison of salary 1 to salary 2 to see who makes more.
 
 
 
