2dfc49bd2626225df621da77a2da3e35bdecb4c2
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-03 22:18:43 -0400

    Add files via upload

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(inputOne *50);
+            //line of code takes user input and multiplies it by 50
+
+            int inputTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(inputTwo + 25);
+            //line of code takes user input and adds it by 25
+
+
+            int inputThree = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(inputThree / 12.5);
+            //line of code takes user input and divides it by 12.5
+
+            bool inputFour = > 50;
+            Console.WriteLine(trueOrFalse.ToString());
+            Console.ReadLine();
+            //line of code takes user input and checks if its greater than 50
+
+
+            int inputFive = (inputFive % 7);
+            Console.WriteLine(inputFive);
+            Console.ReadLine();
+            //line of code takes user input divides it by 7 and prints remainder to console
+
+
+
+
+
+
+
+
+            
+        }
+    }
+}
