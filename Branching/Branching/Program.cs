0eeb400c41795fb7a8422e731e494ca3a181dc43
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-09 14:02:53 -0400

    Add files via upload

--- Branching/Branching/Program.cs
+++ Branching/Branching/Program.cs
@@ -8,47 +8,40 @@ namespace Branching
         {
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
 
-            Console.WriteLine("Please enter the package weight:");
-            int packageweight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package weight:"); //user is prompted to enter package weight
+            int packageWeight = Convert.ToInt32(Console.ReadLine());  //int data type to store package weight
 
-            if (packageweight > 50)
+            if (packageWeight > 50) //if condition statement, if package weight is greater than 50 
             {
-                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day."); //result if package weight is over 50
             }
-            else 
+            else //else statement 
             {
-            Console.WriteLine("Please enter the package width:");
-             int packageheight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package width:"); //user enters package height
+                int packageHeight = Convert.ToInt32(Console.ReadLine()); //int data type to store package height
 
-            Console.WriteLine("Please enter the package height:");
-             int packagewidth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package height:"); //user enters package width
+                int packageWidth = Convert.ToInt32(Console.ReadLine()); //int data type to store package width
 
-            Console.WriteLine("Please enter the package length:");
-             int packagelength = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package length:"); //user enters package length
+                int packageLength = Convert.ToInt32(Console.ReadLine()); //int data type to store package length
 
-                int dimension = Convert.ToInt32(Console.ReadLine());
+                int dimension = packageHeight + packageWidth + packageLength; //defines dimension 
+                if (dimension < 50)  
+                {
+
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+                else
+                {
+                    int result = (packageHeight * packageWidth * packageLength * packageWeight) / 100;
+
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + result / 100);
+                    Console.WriteLine("Thank you!");
+                    Console.ReadLine();
+
+                }
             }
-            
-            result = packageheight * packagewidth * packagelength;
-            Console.WriteLine(result * weight) / 100;
-
-            { Console.WriteLine("Package too big to be shipped via Package Express.");
-           
-            else Console.WriteLine(result / 100);
-         
-             Console.WriteLine("Your estimated total for shipping this package is: " + result / 100);
-            }
-            Console.WriteLine("Thank you!");
-            Console.ReadLine();
-
-
-
-
-
-
-
-
-
         }
     }
 }
