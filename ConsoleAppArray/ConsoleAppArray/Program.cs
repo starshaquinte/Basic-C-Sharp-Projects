2d2cfc9accb2dffcaf957e4401748366e6b500d5
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-11 10:58:01 -0400

    Add files via upload

---
+++ ConsoleAppArray/ConsoleAppArray/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+
+        Console.WriteLine("Select an index of the array:");
+
+        Console.ReadLine();
+        string[] stringArray = new string[4];
+
+        stringArray[0] = "somewhere";
+        stringArray[1] = "over";
+        stringArray[2] = "the";
+        stringArray[3] = "rainbow";
+
+        Console.WriteLine(stringArray);
+        Console.ReadLine();
+
+
+
+        int[] numArray = new int[] { 46, 24, 75, 468, 763 };
+        Console.WriteLine("Select an index of the array");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (stringArray >= 0 && <[4]
+        {
+            Console.WriteLine(string[] stringArray = new string[4];)
+         }
+
+        else
+        {
+            Console.WriteLine("The index you selected does not exist");
+
+        }
+        List<string> intList = new List<string>();
+        intList.Add("My");
+        intList.Add("Name");
+        intList.Add("is");
+        intList.Add("Starsha");
+
+        Console.WriteLine("Select an index of the list: ");
+        Console.ReadLine();
+
+
+
+
+    }
+    }
+
