309d66a447a87c87fc1a0b56fcf2e27f82953e03
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-12 16:25:56 -0400

    Add files via upload

---
+++ ConsoleAppPartOne/ConsoleAppPartOne/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string[] stringArray = new string[5];
+
+        stringArray[0] = "I";
+        stringArray[1] = "am";
+        stringArray[2] = "eating";
+        stringArray[3] = "an";
+        stringArray[4] = "apple";
+
+        Console.WriteLine("Please input some text.");
+        string text = Console.ReadLine();
+
+        foreach (string text in stringArray);
+        {
+            Console.WriteLine(stringArray + [text]);
+        }
+        {
+            Console.WriteLine(stringArray[]);
+        }
+
+        }
+    }
+
