08a461b7d033c32ee85180d72634aa2705c0ded2
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-16 16:46:14 -0400

    Add files via upload

--- ConsoleAppPartOne/ConsoleAppPartOne/Program.cs
+++ ConsoleAppPartOne/ConsoleAppPartOne/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
         static void Main(string[] args)
         {
         //Part One
-        string[] stringArray = new string[5];
+        string[] stringArray = new string[5]; //A one demensional array of strings
 
         stringArray[0] = "I";
         stringArray[1] = "am";
@@ -15,22 +15,22 @@ using System.Collections.Generic;
         stringArray[3] = "an";
         stringArray[4] = "apple";
 
-        Console.WriteLine("Please input some text.");
+        Console.WriteLine("Please input some text."); //Asking the user to input text
         string text = Console.ReadLine();
-        for (int i = 0; i < stringArray.Length; i++)
+        for (int i = 0; i < stringArray.Length; i++) //for loop that iterates through eah string in the array
         {
-            stringArray[i] = stringArray[i] + text;
+            stringArray[i] = stringArray[i] + text; //adds the users text input to the end of each string 
         }
-        foreach (var item in stringArray)
+        foreach (var item in stringArray) //Second loop
         {
-            Console.WriteLine(item);
+            Console.WriteLine(item); //prints off each string in the array one at a time
 
         }
 
         //Part Two
 
         int count = 0;
-        while (count  < 20)//changed condition to check count variable 
+        while (count  < 20)//changed condition to check count variable (to fix infinite loop)
         {
             count += 1;
             Console.WriteLine(count);
@@ -38,56 +38,57 @@ using System.Collections.Generic;
 
         //Part Three
         int count2 = 1;
-         while (count2 < 57)
+         while (count2 < 57) //while loop where the less than comparison is used to determine whether to continue iterating the loop
         {
             Console.WriteLine(count2);
             count2++;
         }
         int count3 = 5;
-        while (count3 <= 10)
+        while (count3 <= 10) //while loop where the less than or equal to comparison is used to determine whether to continue iterating the loop
         {
             Console.WriteLine(count3);
+            count3++;
         }
 
         //Part Four
         List<string> flower = new List<string>()
         {
-            "Carnation", "Orchid", "Rose", "Lily", "Tulip"
+            "Carnation", "Orchid", "Rose", "Lily", "Tulip" //String List where each item in the string is unique
         };
-        Console.WriteLine("Please choose a flower: ");
+        Console.WriteLine("Please choose a flower: "); //asking user to input text to choose a flower
         string flowerChoice = Console.ReadLine();
 
         if (flower.Contains(flowerChoice))
         {
-            for (int i = 0; i < flower.Count; i++)
+            for (int i = 0; i < flower.Count; i++) //for loop that iterates through the list and then displays the index of the item that matches
             {
-                if(flowerChoice == flower[i])
+                if(flowerChoice == flower[i])//checks if the user put in text that isn't on the list
                 {
-                    Console.WriteLine(flower[i] + " is at index " + i);
+                    Console.WriteLine(flower[i] + " is at index " + i);//tells user where their choice is on the index
                     break;
                 }
             }
         }
         else
         {
-            Console.WriteLine("Your choice was not in the list.");
+            Console.WriteLine("Your choice was not in the list."); //tells user that their choice isn't on the list 
 
         }
         //Part Five 
         List<string> states = new List<string>()
         {
-            "Georgia", "Connecticut", "Nevada", "Oregon", "Georgia", "Texas"
+            "Georgia", "Connecticut", "Nevada", "Oregon", "Georgia", "Texas" //List of string with 2 identical items 
         };
-        Console.WriteLine("Please choose a state: ");
+        Console.WriteLine("Please choose a state: "); //Ask the user to choose a state 
         string stateChoice = Console.ReadLine();
 
         if (states.Contains(stateChoice))
         {
-            for (int i = 0; i < states.Count; i++)
+            for (int i = 0; i < states.Count; i++) //for loop that iterates through the list
             {
                 if (stateChoice == states[i])
                 {
-                    Console.WriteLine(states[i] + " is at index " + i);
+                    Console.WriteLine(states[i] + " is at index " + i);//displays indices of the items matching the user's selection
                 }
 
             }
@@ -95,18 +96,23 @@ using System.Collections.Generic;
         //Part Six
         List<string> cars = new List<string>()
         {
-            "CarA", "CarB", "CarC", "CarD", "CarB"
+            "CarA", "CarB", "CarC", "CarD", "CarB" // list of string with 2 identical strings
         };
         List<string> CarLetters = new List<string>();
 
-        foreach (string car in cars)
+        foreach (string car in cars) //for each loop that evaluates each item on the list
         {
-            if cars ==
-         }
-        { 
-            Console.WriteLine("This item is unique");
-        }
+            if (CarLetters.Contains(car))
+            {
+                Console.WriteLine(car + " is a duplicate"); //message to display the string is a duplicate
+            }
+            else
+            {
+                Console.WriteLine(car + " item is unique"); //message to display the string is unique
+                CarLetters.Add(car);
 
+            }
+        }
         Console.ReadLine();
 
         }
