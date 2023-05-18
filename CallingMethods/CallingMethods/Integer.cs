c89c2db979529e8f97ed34aeacc8dd8d40f2f62d
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-18 17:37:28 -0400

    Add files via upload

---
+++ CallingMethods/CallingMethods/Integer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Integer //created a class named Integer
+    {
+        public int addition(int num) //access modifier that the method can be called publically - created a method called integer- using the int num parameters
+        {
+            int result = num + num; //addition operation using integer data type
+            return result; //return statement, returns the value back from the addition operation
+        }
+
+        public int subtraction(int num2)
+        {
+            int result = num2 - num2; //subtraction operation using integer data type
+            return result;
+        }
+
+        public int multiplication(int num3)
+        {
+            int result = num3 * num3;
+            return result;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Integer obj = new Integer();
+
+            Console.WriteLine("Please enter a number. ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(obj.addition(userInput));
+            
+
+            Console.WriteLine(obj.subtraction(userInput));
+            
+
+            Console.WriteLine(obj.multiplication(userInput));
+            Console.ReadLine();
+
+        }
+
+        
+        
+    }
+}
