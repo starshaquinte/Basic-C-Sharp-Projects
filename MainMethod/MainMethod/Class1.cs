4b08270b1fdf96fbcee04a07f1b4b64d186f0ec0
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-19 14:52:11 -0400

    Add files via upload

---
+++ MainMethod/MainMethod/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Class1
+    {
+        public int addition(int num, int num1) //class atributes
+        {
+            int result = num + num1;
+            return result;
+        }
+
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1(); //creating a new object called class1
+            obj.addition = 45, 35;
+
+            Console.WriteLine(obj.addition);
+            Console.ReadLine();
+
+
+            
+
+                
+    }
+        }
+    }
+
