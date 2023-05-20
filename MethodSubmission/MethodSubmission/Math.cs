031c80f7c8c5e37528d932eaec20283243ca64f2
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-19 21:17:12 -0400

    Add files via upload

---
+++ MethodSubmission/MethodSubmission/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class Math
+    {
+        public int Addition(int num, int num2 = 1) //created a method Addition called it data type int num and num2 - made num2 optional with the = 1
+        {
+            int result = num + num2; // math operation equals result
+            return result; //returns an int result back to the main program
+
+        }
+    }
+}
---
+++ MethodSubmission/MethodSubmission/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math obj = new Math(); //instantiated class object
+            Console.WriteLine("Please enter a number:"); //Ask the user to enter a number 
+            int num = Convert.ToInt32(Console.ReadLine()); //the user's answer( takes int the input and assigns it to num)
+            try //try block  
+            {
+                Console.WriteLine("Enter a 2nd number if you want:"); //Give the user an option to enter a 2nd number
+                int num2 = Convert.ToInt32(Console.ReadLine()); //takes in the user's answer (assigned int to num2)
+                Console.WriteLine(obj.Addition(num, num2)); //adds num and num 2 and writes it to the console
+
+            }
+            catch //catch block
+            {
+                Console.WriteLine(obj.Addition(num)); //if the user does not enter a number program prints out object method num (our original value)
+            }
+            Console.ReadLine(); //print final result to the screen
+        }
+    }
+}
