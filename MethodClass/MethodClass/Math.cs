d1d2ce65fcef61ff52e0cf3de8febec58adacad4
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-21 12:59:40 -0400

    Add files via upload

---
+++ MethodClass/MethodClass/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Math
+    {
+        public void CallingMethods(int num, int num2)
+        {
+            int result = num + 7;
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math obj = new Math();
+
+            Console.WriteLine(obj.CallingMethods(8, 4));
+            Console.WriteLine(obj.CallingMethods(num1 + num2));
+            Console.ReadLine();
+
+        }
+    }
+}
