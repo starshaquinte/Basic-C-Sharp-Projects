0170d41e8e1bfe1ca07e769aae41eb98f59a97a8
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-22 14:21:25 -0400

    Add files via upload

---
+++ ClassSubmission/ClassSubmission/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            division obj = new division();
+            Console.WriteLine("Please enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            obj.Math(num1);
+            int num2 = 0;
+            obj.Math(out num1, out num2);
+            Console.WriteLine("num1 is now " + num1);
+            Console.WriteLine("num2 is now " + num2);
+            MyStaticClass.MyStaticMethod();
+
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
---
+++ ClassSubmission/ClassSubmission/division.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    class division
+
+    {
+        public void Math(int num1) //This is my divide by 2 method 
+        {
+            int result = num1 / 2;
+            Console.WriteLine(result);
+            
+        }
+
+        public void Math(out int num1, out int num2) //Output parameters method
+        {
+            num1 = 6;
+            num2 = 2;
+        }
+    }
+
+    public static class MyStaticClass //My static class
+    {
+        public static void MyStaticMethod() //static method
+        {
+            Console.WriteLine("This message is for my static class");
+        }
+    }
+}
