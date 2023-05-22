f815f420fa31fb04b8ed9570d0f7408bea2ed7a0
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-22 18:11:29 -0400

    Add files via upload

--- MethodClass/MethodClass/Math.cs
+++ MethodClass/MethodClass/Math.cs
@@ -8,10 +8,17 @@ namespace MethodClass
 {
     class Math
     {
-        public void CallingMethods(int num, int num2)
+        public void CallingMethods(int num, int num2) 
         {
             int result = num + 7;
             Console.WriteLine(num2);
         }
+
+    }public int Addition(int num, int num2 = 1)//created a class w/ a void method  takes two integers as parameters
+    {
+        int result = num + num2; //math operation done on the first integer
+        Console.WriteLine(num2);
+        return result;
     }
+
 }
