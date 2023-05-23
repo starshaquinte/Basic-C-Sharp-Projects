29b1f518c11027c777509e27fe5126d3532f92e6
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-23 19:18:47 -0400

    Add files via upload

---
+++ Polymorphism/Polymorphism/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Employee : IQuittable //inheriting IQuittable interface
+    {
+        public void Quit() //implementing quit method from interface IQuittable 
+        {
+            Console.WriteLine("Employee quit");
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable quitter = new Employee(); //object of type IQuittable
+            quitter.Quit(); //calling quit method w/ quitter
+
+        }
+    }
+}
