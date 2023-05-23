76a04f70945adf8488fdf684fa57868e2358fbbe
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-23 10:30:39 -0400

    Add files via upload

---
+++ MethodsAndObjects/MethodsAndObjects/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+    }
+
+    public static void SayName()
+    {
+        Console.WriteLine("Name: [full name]");
+
+    }
+}
---
+++ MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person obj = new Person();
+
+            Employee employee = new Employee();
+            FirstName.Sample();
+            LastName.Student();
+        }
+    }
+}
