fc21313ced7e345cabab7e6e25bacc26aa0f869f
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-24 13:25:55 -0400

    Add files via upload

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator ==(Employee emp1, Employee emp2) //overloaded == operator 
+        {
+            return emp1.Id == emp2.Id; //checks if employee ids are equal
+
+        }
+
+        public static bool operator !=(Employee emp1, Employee emp2) //overloaded != operator 
+        {
+            return emp1.Id != emp2.Id; //checks if employee ids are not equal
+        }
+
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating two employee objects
+            Employee employee = new Employee();
+            Employee employee2 = new Employee(); 
+            //assigning employee ids
+            employee.Id = 1;
+            employee2.Id = 2;
+            Console.WriteLine(employee == employee2); //testing overload == operator to see if it works correctly
+            Console.WriteLine(employee != employee2); //testing !=
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
