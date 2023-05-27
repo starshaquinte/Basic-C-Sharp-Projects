840772589ac1ced27d64187914cbc2fc824ab67d
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-27 18:14:05 -0400

    Add files via upload

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           DateTime currentTime = DateTime.Now()
+           Console.WriteLine("Current date and time: " + currentTime);
+           Console.WriteLine("Please enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            DateTime futureTime = currentTime.AddHours(num);
+            Console.WriteLine("It will be " + futureTime in num hours.);
+
+        }
+    }
+}
