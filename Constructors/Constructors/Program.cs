298d8988b0eec083002b923349b399d49304b427
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-27 22:08:55 -0400

    Add files via upload

---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        const int a = 70;
+        static void Main(string[] args)
+        {
+            var count = 37;
+        }
+            public int num;
+        public string hello;
+
+        public Constructors(int num, string hello)
+        {
+            this.num = num;
+            this.hello = hello;
+
+        }
+        }
+    }
+
