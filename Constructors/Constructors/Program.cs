f4fdbeb159602afd03b40f2d8f86affb484279ba
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-05-30 14:44:10 -0400

    Add files via upload

--- Constructors/Constructors/Program.cs
+++ Constructors/Constructors/Program.cs
@@ -4,20 +4,34 @@ namespace Constructors
 {
     class Program
     {
-        const int a = 70;
+        
         static void Main(string[] args)
         {
-            var count = 37;
+            const int a = 70; //created a const variable
+            var count = 37; //created a variable using the var keyword
+            Console.WriteLine("Here is my const variable value: " + a); //Wrote const to the console
+            Console.WriteLine("Here is my var variable value: " + count); //Wrote var to the console
+            MyConstructor obj = new MyConstructor(); // calling the method
+            Console.WriteLine(obj.num + " " + obj.hello); //writing object num and object hello to the console
+            Console.ReadLine();
         }
-            public int num;
+        
+    }
+    public class MyConstructor
+    {
+        public int num;
         public string hello;
 
-        public Constructors(int num, string hello)
+        public MyConstructor(int num, string hello)
         {
-            this.num = num;
-            this.hello = hello;
+            this.num = num; //1st constructor takes an integer num
+            this.hello = hello; //2nd constructor takes a string hello
 
         }
+        public MyConstructor() : this(8, "Hello") // chaining two constructors together
+        {
+
         }
     }
+}
 
