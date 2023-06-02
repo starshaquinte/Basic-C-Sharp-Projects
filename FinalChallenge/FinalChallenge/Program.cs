c5f160ae3dc9b9a5de60dcc2fbf33ebe0869569d
starshaquinte <126527633+starshaquinte@users.noreply.github.com>
2023-06-02 16:21:48 -0400

    Final Challenge

---
+++ FinalChallenge/FinalChallenge/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FinalChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new StudentContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ FinalChallenge/FinalChallenge/StudentContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FinalChallenge
+{
+    public class StudentContext : DbContext
+    {
+        //public DbSet<Student> Blogs { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
